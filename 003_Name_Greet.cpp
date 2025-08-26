d72d5d89f32afbb1c20149f74f2dd1cc6db4ccc4
Aparna K A <aparnaanil636@gmail.com>
2025-08-26 21:15:26 +0530

    Add .gitignore to ignore .exe files

---
+++ 003_Name_Greet.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // char name;
+    string name;
+    cout<<"Enter your name:";
+    cin>>name;
+    cout<<"Nama Shivaya"<<" "<<name<<"!"<<"\n";
+
+}
