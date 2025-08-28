154d75e33f89b789db1df713f99aa0746c12ec9d
Aparna K A <aparnaanil636@gmail.com>
2025-08-28 14:51:31 +0530

    Create 017_Circumference.cpp

---
+++ 017_Circumference.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    double r;
+    cout<<"enter the radius of circle : ";
+    cin>>r;
+    double circumference=2*3.14*r;
+    cout<<"circumference = "<<circumference;
+    return 0;
+}
