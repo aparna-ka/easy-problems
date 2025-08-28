dfb5230b8de1e39610105f03328bbef56f91e55f
Aparna K A <aparnaanil636@gmail.com>
2025-08-28 13:37:16 +0530

    Create 015_Cube.cpp

---
+++ 015_Cube.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"Enter a number: \n";
+    cin>>a;
+    int cube= a*a*a;
+    cout<<"Cube of "<<a<<" is "<<cube;
+    return 0;
+
+
+}
