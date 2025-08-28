090fe431b690d77c4a22ef6198797a320a882f41
Aparna K A <aparnaanil636@gmail.com>
2025-08-28 13:18:07 +0530

    Create 011_Average.cpp

---
+++ 011_Average.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float a,b,c;
+    cout<<"enter three numbers";
+    cin>>a>>b>>c;
+    float avg=(a+b+c)/3;
+    cout<<"average is "<<avg;
+    return 0;
+
+}
