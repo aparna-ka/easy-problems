b899b59b42e9a7e2dbacba95be9f6f68bbea7ac9
Aparna K A <aparnaanil636@gmail.com>
2025-08-28 15:52:34 +0530

    Create 020_ASCII.cpp

---
+++ 020_ASCII.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char ch;
+    cout<<"enter a character : \n";
+    cin>>ch;
+    cout<<"ASCII Value of"<<ch<<" is "<<int(ch);
+    return 0;
+}
