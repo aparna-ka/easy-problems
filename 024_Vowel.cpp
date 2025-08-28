2121f78a7ea0b3a0bfdc48292f21ee98abae4901
Aparna K A <aparnaanil636@gmail.com>
2025-08-28 22:25:24 +0530

    Create 024_Vowel.cpp

---
+++ 024_Vowel.cpp
@@ -0,0 +1,18 @@
+//vowel or constant
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    char ch;
+    cout<<"enter a character : \n";
+    cin>>ch;
+    if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' || ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U'){
+
+        cout<<ch<<" is vowel";
+    }
+    else 
+        cout<<ch<<" is consonants";
+    
+    return 0;
+}
