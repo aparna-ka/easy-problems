f1d5e2f49495e67cb7b9d6b4e51213dbc64428be
Aparna K A <aparnaanil636@gmail.com>
2025-08-28 13:23:45 +0530

    Create 012_Largest.cpp

---
+++ 012_Largest.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter two numbers";
+    cin>>a>>b;
+    if(a>b)
+    cout<<"Larger is "<<a;
+    else
+    cout<<"Larger is "<<b;
+    return 0;
+}
