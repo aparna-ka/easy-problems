52683ae9dab500e4e03c5ad548a583df058c3461
Aparna K A <aparnaanil636@gmail.com>
2025-08-28 13:31:03 +0530

    Create 014_Square.cpp

---
+++ 014_Square.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"Enter a number: \n";
+    cin>>a;
+    int sqr= a*a;
+    cout<<"Squar of "<<a<<" is "<<sqr;
+    return 0;
+
+
+}
