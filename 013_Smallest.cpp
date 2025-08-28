921939352054c1e2c92eda984a0c2caeb2afbec5
Aparna K A <aparnaanil636@gmail.com>
2025-08-28 13:26:46 +0530

    Create 013_Smallest.cpp

---
+++ 013_Smallest.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter two numbers";
+    cin>>a>>b;
+    if(a<b)
+    cout<<"Smaller is "<<a;
+    else
+    cout<<"Smaller is "<<b;
+    return 0;
+}
