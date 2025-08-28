744e42e709d602cb0f12c8bf5a9685068286f780
Aparna K A <aparnaanil636@gmail.com>
2025-08-28 14:46:39 +0530

    Create 016_Area_Circle.cpp

---
+++ 016_Area_Circle.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int r;
+    cout<<"enter the radius of circle : ";
+    cin>>r;
+    float area=3.14*r*r;
+    cout<<"Area = "<<area;
+    return 0;
+}
