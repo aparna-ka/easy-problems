98568db8d912a4770712f2bbbf3483f6011c5ccf
Aparna K A <aparnaanil636@gmail.com>
2025-09-14 22:03:30 +0530

    Create 058_Area_Rectangle.cpp

---
+++ 058_Area_Rectangle.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float l,b,area;
+    cout<<"Enter length: ";
+    cin>>l;
+    cout<<"Enter breadth: ";
+    cin>>b;
+    area=l*b;
+    cout<<"Area of rectangle :"<<area;
+}
